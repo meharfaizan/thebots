b345437bcdcccba6cb6b9f955641f4e894be6eda
meharfaizan <87762660+meharfaizan@users.noreply.github.com>
2023-02-25 12:32:56 +0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,25 +1,17 @@
-#    This file is part of the Compressor distribution.
 #    Copyright (c) 2021 Danish_00
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, version 3.
-#
-#    This program is distributed in the hope that it will be useful, but
-#    WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-#    General Public License for more details.
-#
-# License can be found in <
-# https://github.com/1Danish-00/CompressorQueue/blob/main/License> .
+#    Improved By @Zylern
 
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID","6534707", cast=int)
+    APP_ID = config("APP_ID", "6534707",cast=int)
     API_HASH = config("API_HASH","4bcc61d959a9f403b2f20149cbbe627a")
-    BOT_TOKEN = config("BOT_TOKEN","5142296427:AAGe28CHWVme4OJ4v51xxqvNbnPwD8vxQic")
+    BOT_TOKEN = config("BOT_TOKEN","6039293766:AAHUJRrpvlS6zQOvP3eP9Er1XXJcIheWk3U")
     DEV = 1664850827
-    OWNER = config("OWNER",'1430593323')
+    OWNER = config("OWNER","1430593323")
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TGVid-Comp (https://github.com/Zylern/TGVid-Comp)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
-    THUMB = config("THUMBNAIL")
+    THUMBNAIL = config("THUMBNAIL", default="https://telegra.ph/file/f9e5d783542906418412d.jpg")
+except Exception as e:
+    print("Environment vars Missing! Exiting App.")
+    print(str(e))
+    exit(1)
